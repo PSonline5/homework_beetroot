d7c140789c0010bb6e3c9a029f65cb7d22979ca6
Павел <pavel-clever@mail.ru>
2019-09-20 12:11:12 +0300

    homework

---
+++ hmw.py
@@ -0,0 +1,21 @@
+# The greeting program.
+name = ' Pavel'
+day = ' Friday '
+print("Good day" + name + "!" + day + "is a perfect day to learn some python.")
+
+# Manipulate strings.
+first_name = ' Pavel '
+last_name = 'Blabla'
+full_name = first_name + last_name
+print("Good day" + full_name + "!" + day
+      + "is a perfect day to learn some python.")
+# Using python as a calculator.
+a = 1
+b = 2
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a % b)
+print(a//b)
