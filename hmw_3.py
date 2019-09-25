2c57a248a8fe33f9c4545bd29abc8830b3458418
Павел <pavel-clever@mail.ru>
2019-09-25 12:00:05 +0300

    homework3

---
+++ hmw_3.py
@@ -0,0 +1,19 @@
+# first task
+string = "test1, test2, test3, test4, test5"
+string = string.split(',')
+number = string[int(input('Enter the number = '))]
+string.remove(number)
+string = ','.join(string)
+print(string)
+
+# first task with replace
+string = "test1, test2, test3, test4, test5"
+string = string.split(',')
+number = string[int(input('Enter the number = '))]
+string = ','.join(string)
+print(string.replace(f"{number}{','}", ""))
+
+# second task
+# url = input()
+url = 'https://realpython.com/courses/python-thonny/'
+print(url.split('/')[2])
