03bc08704c69175b1f01c6a1df024cfb960cd78a
Павел <pavel-clever@mail.ru>
2019-09-28 14:20:29 +0300

    fix

--- hmw_4.py
+++ hmw_4.py
@@ -1,29 +1,29 @@
 # first task
-# string = input()
-# symbol = input()
-# for i in string:
-#     if i == symbol:
-#         string = string.replace(symbol, '')
-# print(string)
+string = input()
+symbol = input()
+for i in string:
+    if i == symbol:
+        string = string.replace(symbol, '')
+print(string)
 
 # second task
-# names = ['John',  'Kate',  'Dave',  'Den', 'Adele']
-# string1 = input()
-# if string1 not in names:
-#     print('not found')
-# else:
-#     if names.index(string1) % 2 == 0:
-#         print("it's all good")
-#     else:
-#         string1 = names.index(string1)
-#         del names[string1]
-#         print(names)
+names = ['John',  'Kate',  'Dave',  'Den', 'Adele']
+string1 = input()
+if string1 not in names:
+    print('not found')
+else:
+    if names.index(string1) % 2 == 0:
+        print("it's all good")
+    else:
+        string1 = names.index(string1)
+        del names[string1]
+        print(names)
 
 # third task
-# nums = [75, 81, 96, 213, 94, 15, 38, 11]
-# while sum(nums) > 200:
-#     nums.pop()
-# print(nums)
+nums = [75, 81, 96, 213, 94, 15, 38, 11]
+while sum(nums) > 200:
+    nums.pop()
+print(nums)
 
 # fourth task
 number = int(input())
