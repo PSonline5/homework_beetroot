1bedb0b4e5545df199e2f0d522b712ad72e909c9
Павел <pavel-clever@mail.ru>
2019-10-18 19:04:53 +0300

    homework

--- hmw_12.py
+++ hmw_12.py
@@ -4,42 +4,42 @@
 #   "add called with 4, 5"
 
 
-# def logger(func):
-#     def wrapper(*args):
-#         return f"{func} with {args}"
-#     return wrapper
-#
-#
-# @logger
-# def add(x, y):
-#     return x + y
-#
-#
-# @logger
-# def square_all(*args):
-#     return [arg ** 2 for arg in args]
+def logger(func):
+    def wrapper(*args):
+        return f"{func} with {args}"
+    return wrapper
+
+
+@logger
+def add(x, y):
+    return x + y
+
+
+@logger
+def square_all(*args):
+    return [arg ** 2 for arg in args]
 
 # Write a decorator that takes a list of stop words and replaces in them
 # with * inside decorated function
 
 
-# def stop_words(words: list):
-#     def decorator(func):
-#         def wrapper(*args, **kwargs):
-#             x = func(*args, **kwargs)
-#             for word in words:
-#                 x = x.replace(word, "*")
-#             return x
-#         return wrapper
-#     return decorator
-#
-#
-# @stop_words(['pepsi', 'BMW'])
-# def create_slogan(name: str) -> str:
-#     return f"{name} drinks pepsi in his brand new BMW!"
-#
-#
-# assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
+def stop_words(words: list):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            x = func(*args, **kwargs)
+            for word in words:
+                x = x.replace(word, "*")
+            return x
+        return wrapper
+    return decorator
+
+
+@stop_words(['pepsi', 'BMW'])
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+
+assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
 
 # Write a decorator arg_rules that validates arguments passed to the function
 # A decorator should take 3 arguments:
